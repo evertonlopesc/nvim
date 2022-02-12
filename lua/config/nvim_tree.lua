bda62f551015366bdec8deac37111e2618822dcd
evertonlopesc <everton.lopess@outlook.com>
2022-02-11 22:35:09 -0300

    Number changed to visible.

--- lua/config/nvim_tree.lua
+++ lua/config/nvim_tree.lua
@@ -50,8 +50,8 @@ require'nvim-tree'.setup {
       custom_only = false,
       list = {}
     },
-    number = false,
-    relativenumber = false,
+    number = true,
+    relativenumber = true,
     signcolumn = "yes"
   },
   trash = {
