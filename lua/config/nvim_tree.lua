2e49fd522df084dbbeef7f2a5f547f0a71040d6d
evertonlopesc <everton.lopess@outlook.com>
2022-02-19 15:47:05 -0300

    chore: hidden root, focused file, and hijack cursor true.

--- lua/config/nvim_tree.lua
+++ lua/config/nvim_tree.lua
@@ -1,5 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
@@ -7,33 +5,20 @@ require'nvim-tree'.setup {
   ignore_ft_on_setup  = {},
   auto_close          = false,
   open_on_tab         = false,
-  hijack_cursor       = false,
+  hijack_cursor       = true,
   update_cwd          = false,
   update_to_buf_dir   = {
     enable = true,
     auto_open = true,
   },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
   update_focused_file = {
-    enable      = false,
+    enable      = true,
     update_cwd  = false,
     ignore_list = {}
   },
   system_open = {
     cmd  = nil,
     args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
   },
   git = {
     enable = true,
@@ -43,7 +28,7 @@ require'nvim-tree'.setup {
   view = {
     width = 30,
     height = 30,
-    hide_root_folder = false,
+    hide_root_folder = true,
     side = 'left',
     auto_resize = true,
     mappings = {
