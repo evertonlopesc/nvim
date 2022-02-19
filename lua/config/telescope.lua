1ea99569ffe235386fa2abf669d0c2015584c93c
evertonlopesc <everton.lopess@outlook.com>
2022-02-19 16:23:49 -0300

    style: added close with ESC and hidde "./".

---
+++ lua/config/telescope.lua
@@ -0,0 +1,26 @@
+local actions = require("telescope.actions")
+
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close
+      },
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--trim" -- add this value
+    }
+  },
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+    },
+  }
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,6 +40,9 @@ return require('packer').startup(function()
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} },
+    config = function ()
+      require("config.telescope")
+    end
   }
 
   -- Syntax highlight
