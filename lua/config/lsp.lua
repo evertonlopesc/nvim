08f57a6038f3a48947ec0814434ae16859f12651
evertonlopesc <everton.lopess@outlook.com>
2022-03-13 10:37:08 -0300

    style: change string called.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 local nvim_lsp = require('lspconfig')
-local lsp_installer = require("nvim-lsp-installer")
+local lsp_installer = require('nvim-lsp-installer')
 
 local function common_on_attach(bufnr)
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -8,26 +8,26 @@ local function common_on_attach(bufnr)
 end
 
 lsp_installer.on_server_ready(function(server)
-    local opts = {
-      on_attach = common_on_attach,
-      handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(
-          vim.lsp.diagnostic.on_publish_diagnostics, {virtual_text = false}
-        ),
-      },
-    }
+  local opts = {
+    on_attach = common_on_attach,
+    handlers = {
+      ['textDocument/publishDiagnostics'] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {virtual_text = false}
+      ),
+    },
+  }
 
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
+  server:setup(opts)
+  vim.cmd('do User LspAttachBuffers')
 end)
 
 lsp_installer.settings {
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  ui = {
+    icons = {
+        server_installed = '✓',
+        server_pending = '➜',
+        server_uninstalled = '✗'
     }
+  }
 }
 
