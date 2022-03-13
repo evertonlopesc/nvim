b7ab6742abf320947b3763a99c613063ca067249
evertonlopesc <everton.lopess@outlook.com>
2022-03-13 10:39:48 -0300

    feat: add capabilities to lsp.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -31,3 +31,14 @@ lsp_installer.settings {
   }
 }
 
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'solargraph', 'tailwindcss', 'tsserver', 'volar' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    capabilities = capabilities,
+  }
+end
