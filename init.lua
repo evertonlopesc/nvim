a8c8b3fbd25d07d912fbc47b7959f9d52356cd55
evertonlopesc <everton.lopess@outlook.com>
2022-04-16 14:47:31 -0300

    refactor[init]: identify extension.

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require ("bootstrap")
 require ("plugins")
 require ("mappings")
+
+-- Extension
 require ('kommentary.config').use_extended_mappings()
 require ('luasnip').filetype_extend("ruby", {"rails"})
