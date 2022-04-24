dc22a0db24245d92ae5d1a01578e61c6dd4aba6a
evertonlopesc <everton.lopess@outlook.com>
2022-04-24 11:32:21 -0300

    fix: extension remove.

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 require ("bootstrap")
 require ("plugins")
 require ("mappings")
-
--- Extension
-require ('kommentary.config').use_extended_mappings()
-require ('luasnip').filetype_extend("ruby", {"rails"})
