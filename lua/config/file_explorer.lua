0f87d2a8025269cf4d2fc144cfdb697f6b1bca91
evertonlopesc <everton.lopess@outlook.com>
2022-04-27 22:16:04 -0300

    fix[nvim-tree]: remove auto_close, update_to_buf_dir and auto_resize.

--- lua/config/file_explorer.lua
+++ lua/config/file_explorer.lua
@@ -3,14 +3,9 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = true,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   update_focused_file = {
     enable      = true,
     update_cwd  = false,
@@ -30,7 +25,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = true,
     side = 'left',
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {}
