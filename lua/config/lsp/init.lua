b9e1bbb596beb81cefba4fa4056328427a1782d6
evertonlopesc <everton.lopess@outlook.com>
2022-06-02 09:24:40 -0300

    refactor(lsp): exec lint.

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -12,6 +12,29 @@ require("nvim-lsp-installer").setup({
 local lspconfig = require('lspconfig')
 local lsp_protocal = vim.lsp.protocol.make_client_capabilities()
 
+local capabilities = lsp_protocal
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = require 'nvim-lsp-installer.servers'
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+  }
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {
+      spacing = 4,
+    },
+    signs = function(namespace, bufnr)
+      return vim.b[bufnr].show_signs == true
+    end,
+    update_in_insert = false,
+  }
+)
+
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
@@ -37,29 +60,6 @@ lspconfig.sumneko_lua.setup {
     },
   },
 }
-
-local capabilities = lsp_protocal
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-local servers = require 'nvim-lsp-installer.servers'
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    capabilities = capabilities,
-  }
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = {
-      spacing = 4,
-    },
-    signs = function(namespace, bufnr)
-      return vim.b[bufnr].show_signs == true
-    end,
-    update_in_insert = false,
-  }
-)
 
 lspconfig.solargraph.setup{}
 lspconfig.html.setup{}
