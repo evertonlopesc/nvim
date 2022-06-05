d2a7e02f9a24c2ace2895c1e7e794cb78b685769
evertonlopesc <everton.lopess@outlook.com>
2022-06-05 08:51:13 -0300

    refactor(lsp): fix errors and added handlers.

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -9,22 +9,53 @@ require("nvim-lsp-installer").setup({
   }
 })
 
-local on_attach = function()
-  require('lua/config/lsp/mappings')
-end
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>lp', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>lsl', vim.diagnostic.setloclist, opts)
 
-local lspconfig = require('lspconfig')
-local lsp_protocal = vim.lsp.protocol.make_client_capabilities()
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-local capabilities = lsp_protocal
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<leader>ltd', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<leader>le', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
 
-local servers = require 'nvim-lsp-installer.servers'
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function ()
+      local opts = {
+        focusable = false,
+        close_events = { "BuffLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end
+  })
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
@@ -38,6 +69,61 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   end,
   update_in_insert = false,
 })
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "single"
+  }
+)
+
+local function goto_definition(split_cmd)
+  local util = vim.lsp.util
+  local log = require("vim.lsp.log")
+  local api = vim.api
+
+  local handler = function (_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, "No location found")
+      return nil
+    end
+
+    if split_cmd then
+      vim.cmd(split_cmd)
+    end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command("copen")
+        api.nvim_command("wincmd p")
+      end
+
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
+end
+
+vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
+
+local lspconfig = require('lspconfig')
+local lsp_protocal = vim.lsp.protocol.make_client_capabilities()
+
+local capabilities = lsp_protocal
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = require 'nvim-lsp-installer.servers'
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach(),
+    capabilities = capabilities,
+    handlers = handlers
+  }
+end
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
@@ -66,9 +152,9 @@ lspconfig.sumneko_lua.setup {
 }
 
 lspconfig.solargraph.setup {
+  init_options = { formatting = true },
   on_attach = on_attach,
   capabilities = capabilities,
-  formatting = true
 }
 lspconfig.html.setup {}
 lspconfig.emmet_ls.setup {}
