e0d43e70eb1921dadd5521789dd8421400d3f62b
evertonlopesc <everton.lopess@outlook.com>
2022-06-05 06:33:58 -0300

    refactor(lsp): separating responsibility for mappings.

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -9,38 +9,8 @@ require("nvim-lsp-installer").setup({
   }
 })
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<leader>lp', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>lsl', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>ltd', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>le', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
+local on_attach = function()
+  require('lua/config/lsp/mappings')
 end
 
 local lspconfig = require('lspconfig')
@@ -59,16 +29,15 @@ end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = {
-      spacing = 4,
-    },
-    signs = function(namespace, bufnr)
-      return vim.b[bufnr].show_signs == true
-    end,
-    update_in_insert = false,
-  }
-)
+  underline = true,
+  virtual_text = {
+    spacing = 4,
+  },
+  signs = function(namespace, bufnr)
+    return vim.b[bufnr].show_signs == true
+  end,
+  update_in_insert = false,
+})
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
@@ -96,14 +65,20 @@ lspconfig.sumneko_lua.setup {
   },
 }
 
-lspconfig.solargraph.setup{}
-lspconfig.html.setup{}
-lspconfig.emmet_ls.setup{}
-lspconfig.cssls.setup{}
-lspconfig.clangd.setup{}
-lspconfig.jsonls.setup{}
-lspconfig.tsserver.setup{}
-lspconfig.vuels.setup{}
-lspconfig.zk.setup{}
-lspconfig.rust_analyzer.setup{}
-
+lspconfig.solargraph.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  formatting = true
+}
+lspconfig.html.setup {}
+lspconfig.emmet_ls.setup {}
+lspconfig.cssls.setup {}
+lspconfig.clangd.setup {}
+lspconfig.jsonls.setup {}
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+lspconfig.vuels.setup {}
+lspconfig.zk.setup {}
+lspconfig.rust_analyzer.setup {}
