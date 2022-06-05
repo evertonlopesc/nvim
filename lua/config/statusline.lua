6167f7c9e6c491f30dd8c033c1baaa118b00af50
evertonlopesc <everton.lopess@outlook.com>
2022-06-05 08:52:15 -0300

    refactor(statusline): change msg to none.

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -28,7 +28,7 @@ require('lualine').setup {
       {
         -- Lsp server name .
         function()
-          local msg = 'No Active Lsp'
+          local msg = 'none'
           local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
           local clients = vim.lsp.get_active_clients()
           if next(clients) == nil then
