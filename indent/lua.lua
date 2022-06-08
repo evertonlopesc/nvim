460ca63841a463c5b4bff6a8894cc7e59832c0e9
evertonlopesc <everton.lopess@outlook.com>
2022-06-08 00:54:40 -0300

    feat: added indentation for ruby, lua and markdown.

--- indent/lua.lua
+++ indent/lua.lua
@@ -1,7 +1,8 @@
+
 local opt = vim.opt
 local opts = { noremap = true }
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.tabstop = 2
-opt.textwidth = 120
-opt.colorcolumn = "120"
+opt.textwidth = 80
+opt.colorcolumn = "80"
