159e9d29b0da711f337d45ef2f84883decab4c8a
evertonlopesc <everton.lopess@outlook.com>
2022-06-12 19:03:24 -0300

    feat(dashboard): plugin update for lua language.

--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -1,30 +1,28 @@
-vim.g.dashboard_custom_header = {
-'',
-'',
-'',
-'  ⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇         ⡆    ⡆       ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋',
-'       ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇       ⢸⣷⣤⣤⣼⡇       ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁',
-'         ⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣤⣤⣤⣤⣼⣿⣿⣿⣿⣷⣤⣤⣤⣤⣴⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁',
-'          ⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁',
-'            ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁',
-'            ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿',
-'            ⠾⠿⠿⠿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿',
-'                        ⠈⠉⠙⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠋⠉⠁',
-'                              ⠈⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁',
-'                                 ⠘⠿⣿⣿⣿⣿⡿⠃',
-'                                   ⠙⣿⣿⠏',
-'                                    ⢹⡟',
-'',
-'',
-}
+local db = require("dashboard")
 
-vim.g.dashboard_custom_section = {
-  a = { description = { '  New file                       ' }, command = 'enew' },
-  b = { description = { '  Find file                 SPC ff' }, command = 'Telescope find_files' },
-  c = { description = { '  Recent files              SPC fo' }, command = 'Telescope oldfiles' },
-  d = { description = { '  Find Word                 SPC fg' }, command = 'Telescope live_grep' },
-}
-
-vim.g.dashboard_custom_footer = {
-  '',
+db.custom_center = {
+  {
+    icon = ' ',
+    desc = 'New file                     ',
+    shortcut = 'enter',
+    action = 'enew'
+  },
+  {
+    icon = ' ',
+    desc = 'Find file              ',
+    shortcut = 'SPC f f',
+    action = 'Telescope find_files'
+  },
+  {
+    icon = ' ',
+    desc = 'Recent Files           ',
+    shortcut = 'SPC f o',
+    action = 'Telescope oldfiles'
+  },
+  {
+    icon = ' ',
+    desc = 'Find word              ',
+    shortcut = 'SPC f g',
+    action = 'Telescope live_grep'
+  },
 }
