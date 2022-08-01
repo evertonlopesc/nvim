66e3d873cf85b6d554ab6728d9b8dba4d9a567cf
evertonlc <everton.lopess@outlook.com>
2022-08-01 19:12:32 -0300

    fix: remove indent with after add point.

--- indent/ruby.lua
+++ indent/ruby.lua
@@ -5,3 +5,4 @@ opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.tabstop = 2
 opt.textwidth = 100
+opt.indentkeys = "0{,0},0), 0], !^F, o, O, e, :, =end, =else, =elsif, =when, =in, =ensure, =rescue, ==begin, ==end, =private, =protected, =public"
