8f4d3ca9c2022e5f647f20479494caa7e8259426
evertonlc <everton.lopess@outlook.com>
2022-08-07 12:27:58 -0300

    refactor: change keymaps for moving in toggleterm.

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -8,10 +8,10 @@ require("toggleterm").setup {
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<esc>', '<ESC>', opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-LEFT>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-DOWN>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-UP>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-RIGHT>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
