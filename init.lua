26f897be1dc49c3b2f72a2494e343c65c079c9cc
evertonlc <everton.lopess@outlook.com>
2022-08-25 21:35:21 -0300

    comment foldexpr from treesitter.

--- init.lua
+++ init.lua
@@ -34,10 +34,10 @@ vim.opt.wig = {
   "**/.git/*"
 }
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter"}, {
+--[[ vim.api.nvim_create_autocmd({"BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter"}, {
   group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
   callback = function()
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
   end
-})
+}) ]]
