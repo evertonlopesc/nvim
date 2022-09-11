712d5c1850db28771c04fc1bc2ae775a161301ef
evertonlc <everton.lopess@outlook.com>
2022-09-11 15:26:06 -0300

    chore: added surround plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -231,6 +231,16 @@ startup(function()
     end
   }
 
+  use {
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
+
   if packer_bootstrap then
     require('packer').sync()
   end
