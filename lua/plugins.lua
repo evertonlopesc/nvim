4546d6f81f263772c56a1da2a55673acc9528bc4
Everton Lopes <everton.lopess@outlook.com>
2022-09-15 18:54:15 -0300

    style(packer): exec indentation.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -235,9 +235,9 @@ startup(function()
     "kylechui/nvim-surround",
     tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end
   }
 
