1a4e58f172254e26b54e6fe4195a1f7e46f898ab
Everton Lopes <everton.lopess@outlook.com>
2022-09-15 18:53:38 -0300

    feat(lualine): added new colors and added other modes.

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -1,23 +1,27 @@
 local colors = {
-  red = '#ca1243',
-  grey = '#a0a1a7',
-  black = '#383a42',
-  white = '#f3f3f3',
+  red         = '#ca1243',
+  grey        = '#a0a1a7',
+  black       = '#383a42',
+  white       = '#f3f3f3',
   light_green = '#83a598',
-  orange = '#fe8019',
-  green = '#8ec07c',
+  orange      = '#fe8019',
+  green       = '#8ec07c',
+  blue        = '#45AFF8',
+  purple      = '#9B45F8',
 }
 
 local theme = {
-  normal = {
+  normal   = {
     a = { fg = colors.white, bg = colors.black },
     b = { fg = colors.white, bg = colors.grey },
     c = { fg = colors.black, bg = colors.white },
     z = { fg = colors.white, bg = colors.black },
   },
-  insert = { a = { fg = colors.black, bg = colors.light_green } },
-  visual = { a = { fg = colors.black, bg = colors.orange } },
-  replace = { a = { fg = colors.black, bg = colors.green } },
+  insert   = { a = { fg = colors.black, bg = colors.light_green } },
+  visual   = { a = { fg = colors.black, bg = colors.orange } },
+  replace  = { a = { fg = colors.black, bg = colors.green } },
+  terminal = { a = { fg = colors.white, bg = colors.blue } },
+  command  = { a = { fg = colors.white, bg = colors.purple } },
 }
 
 local empty = require('lualine.component'):extend()
@@ -136,7 +140,7 @@ require('lualine').setup {
     },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = { search_result, { icon= '力', lsp_server_name }, 'filetype' },
+    lualine_y = { search_result, { icon = '力', lsp_server_name }, 'filetype' },
     lualine_z = { '%l:%c', '%p%%/%L' },
   },
   inactive_sections = {
