ea246409ed66d28bae154e1c531d71b2fcd70d9c
Everton Lopes <everton.lopess@outlook.com>
2022-09-19 11:37:00 -0300

    refactor(lsp): added servers default with on_attach.

--- plugin/lsp/init.lua
+++ plugin/lsp/init.lua
@@ -1,18 +1,7 @@
 local mason = require("mason")
 local mason_lspc = require("mason-lspconfig")
-
-local lspconfig = require('lspconfig')
-local lsp_protocal = vim.lsp.protocol.make_client_capabilities()
-local lsp_handlers = vim.lsp.handlers
-
-local nl = require('null-ls')
-local formatting = nl.builtins.formatting
-local diagnostics = nl.builtins.diagnostics
-local code_actions = nl.builtins.code_actions
-local completion = nl.builtins.completion
-
-local keymap = vim.keymap.set
-local opts = {noremap = true, silent = true}
+local lspc = require("lspconfig")
+local nls = require("null-ls")
 
 mason.setup({
         ui = {
@@ -25,82 +14,52 @@ mason.setup({
 })
 
 mason_lspc.setup({
-        ensure_installed = {'sumneko_lua', 'solargraph', 'tsserver'},
-        automatic_installation = true
+        ensure_installed = {
+                "sumneko_lua",
+                "solargraph",
+                "tsserver",
+        },
+
+        automactic_installation = true,
 })
 
-local sources = {
-        diagnostics.luacheck,
-        diagnostics.rubocop,
-        diagnostics.erb_lint,
-        diagnostics.eslint,
+-- Mappings.
+local keymap = vim.keymap.set
 
-        formatting.rubocop,
-        formatting.erb_lint,
-        formatting.prettierd,
-        formatting.markdownlint,
-        formatting.stylua.with({
-                extra_args = function(params)
-                        local cfg = vim.fn.stdpath("config") .. "/.stylua.toml"
-
-                        if vim.fn.filereadable(params.root .. "/.stylua.toml") == 1 then
-                                cfg = params.root .. "/.stylua.toml"
-                        elseif vim.fn.filereadable(params.root .. "/stylua.toml") == 1 then
-                                cfg = params.root .. "/stylua.toml"
-                        end
-
-                        return { "--config-path", cfg }
-                end,
-        }),
-
-        code_actions.eslint,
-        completion.luasnip
-}
-
-keymap('n', '<leader>lp', vim.diagnostic.open_float, opts)
+local opts = { noremap = true, silent = true }
+keymap('n', '<space>e', vim.diagnostic.open_float, opts)
 keymap('n', '[d', vim.diagnostic.goto_prev, opts)
 keymap('n', ']d', vim.diagnostic.goto_next, opts)
-keymap('n', '<leader>lsl', vim.diagnostic.setloclist, opts)
+keymap('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        local vlsp = vim.lsp.buf
 
-        local bufopts = {noremap = true, silent = true, buffer = bufnr}
-
-        keymap('n', '<leader>lD', vim.lsp.buf.declaration, bufopts)
-        keymap('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
-        keymap('n', '<leader>lh', vim.lsp.buf.hover, bufopts)
-        keymap('n', '<leader>ls', vim.lsp.buf.signature_help, bufopts)
-        keymap('n', '<leader>ltd', vim.lsp.buf.type_definition, bufopts)
-        keymap('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
-        keymap('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
-        keymap('n', '<leader>le', vim.lsp.buf.references, bufopts)
-        keymap('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
-
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-        if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePost", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                                vim.lsp.buf.formatting_sync()
-                        end,
-                })
-        end
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        keymap('n', '<leader>lD', vlsp.declaration, bufopts)
+        keymap('n', '<leader>ld', vlsp.definition, bufopts)
+        keymap('n', 'K', vlsp.hover, bufopts)
+        keymap('n', '<leader>li', vlsp.implementation, bufopts)
+        keymap('n', '<C-k>', vlsp.signature_help, bufopts)
+        keymap('n', '<leader>la', vlsp.add_workspace_folder, bufopts)
+        keymap('n', '<leader>lr', vlsp.remove_workspace_folder, bufopts)
+        keymap('n', '<leader>ll', function()
+                print(vim.inspect(vlsp.list_workspace_folders()))
+        end, bufopts)
+        keymap('n', '<leader>lt', vlsp.type_definition, bufopts)
+        keymap('n', '<leader>lr', vlsp.rename, bufopts)
+        keymap('n', '<leader>lc', vlsp.code_action, bufopts)
+        keymap('n', 'gr', vlsp.references, bufopts)
+        keymap('n', '<leader>lf', vlsp.formatting, bufopts)
 end
 
-local capabilities = lsp_protocal
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-local opts = {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
+lspc['sumneko_lua'].setup {
+        on_attach = on_attach,
 }
-
-nl.setup({ sources = sources })
+lspc['solargraph'].setup {
+        on_attach = on_attach,
+}
+lspc['tsserver'].setup {
+        on_attach = on_attach,
+}
