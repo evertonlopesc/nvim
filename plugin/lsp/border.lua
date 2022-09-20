79f86bcc017dd1365c23e29b2124310958c6038e
Everton Lopes <everton.lopess@outlook.com>
2022-09-20 20:56:08 -0300

    feat: added lsp-ui componentes.

---
+++ plugin/lsp/border.lua
@@ -0,0 +1,24 @@
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = {
+      {"🭽", "FloatBorder"},
+      {"▔", "FloatBorder"},
+      {"🭾", "FloatBorder"},
+      {"▕", "FloatBorder"},
+      {"🭿", "FloatBorder"},
+      {"▁", "FloatBorder"},
+      {"🭼", "FloatBorder"},
+      {"▏", "FloatBorder"},
+}
+
+-- LSP settings (for overriding per client)
+local handlers =  {
+  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+}
+
+-- Do not forget to use the on_attach function
+require 'lspconfig'.sumneko_lua.setup { handlers=handlers }
+require 'lspconfig'.solargraph.setup { handlers=handlers }
+require 'lspconfig'.tsserver.setup { handlers=handlers }
--- plugin/lsp/init.lua
+++ plugin/lsp/init.lua
@@ -55,6 +55,11 @@ local on_attach = function(client, bufnr)
 end
 
 lspc['sumneko_lua'].setup {
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
         on_attach = on_attach,
 }
 lspc['solargraph'].setup {
