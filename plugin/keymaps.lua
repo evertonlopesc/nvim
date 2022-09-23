fcf375fe3bf1f804fe6d2f0268d9198d3a27966e
Everton Lopes <everton.lopess@outlook.com>
2022-09-23 09:56:36 -0300

    style: exec lint.

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -3,7 +3,7 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
 -- Reload file lua
-keymap('n', '<leader>rr', ":source % <CR>", {})
+keymap('n', '<leader>rr', ":source % <CR>", opts)
 
 -- TrailingWhiteSpace
 keymap('n', '<F2>', ":%s/\\s*$//<CR>", opts)
@@ -19,13 +19,13 @@ keymap('i', '<A-h>', "<C-\\><C-n><C-w>h", opts)
 keymap('i', '<A-j>', "<C-\\><C-n><C-w>j", opts)
 keymap('i', '<A-k>', "<C-\\><C-n><C-w>k", opts)
 keymap('i', '<A-l>', "<C-\\><C-n><C-w>l", opts)
-keymap('n', '<A-h>', "<C-w>h", {})
-keymap('n', '<A-j>', "<C-w>j", {})
-keymap('n', '<A-k>', "<C-w>k", {})
-keymap('n', '<A-l>', "<C-w>l", {})
+keymap('n', '<A-h>', "<C-w>h", opts)
+keymap('n', '<A-j>', "<C-w>j", opts)
+keymap('n', '<A-k>', "<C-w>k", opts)
+keymap('n', '<A-l>', "<C-w>l", opts)
 
 -- all select
-keymap('n', ',ca', 'ggVGy')
+keymap('n', ',ca', 'ggVGy', opts)
 
 -- open terminal
 keymap('t', '<A-h>', "<C-\\><C-n><C-w>h", opts)
@@ -34,101 +34,101 @@ keymap('t', '<A-k>', "<C-\\><C-n><C-w>k", opts)
 keymap('t', '<A-l>', "<C-\\><C-n><C-w>l", opts)
 
 -- save files
-keymap('n', '<leader>w', ':w<CR>', {})
-keymap('n', '<leader>W', ':wa<CR>', {})
+keymap('n', '<leader>w', ':w<CR>', opts)
+keymap('n', '<leader>W', ':wa<CR>', opts)
 
 -- close buffer(s)
-keymap('n', '<leader>x', ':q<CR>', {})
-keymap('n', '<leader>X', ':qa<CR>', {})
+keymap('n', '<leader>x', ':q<CR>', opts)
+keymap('n', '<leader>X', ':qa<CR>', opts)
 
 -- open/close highlight
-keymap('n', '<leader><space>', ':set hlsearch!<CR>', {})
+keymap('n', '<leader><space>', ':set hlsearch!<CR>', opts)
 
 -- Buffers
-keymap('n', '<leader>d', ':bdelete<CR>', {})
-keymap('n', '<F10>', ':BufferLineCycleNext<CR>')
-keymap('n', '<F9>', ':BufferLineCyclePrev<CR>')
-keymap('n', '<leader>bb', ':BufferLinePick<CR>', {})
-keymap('n', ',d', ':b#<bar>bd#<CR>')
-keymap('n', ',1', ':BufferLineGoToBuffer 1<CR>')
-keymap('n', ',2', ':BufferLineGoToBuffer 2<CR>')
-keymap('n', ',3', ':BufferLineGoToBuffer 3<CR>')
-keymap('n', ',4', ':BufferLineGoToBuffer 4<CR>')
-keymap('n', ',5', ':BufferLineGoToBuffer 5<CR>')
-keymap('n', ',6', ':BufferLineGoToBuffer 6<CR>')
-keymap('n', ',7', ':BufferLineGoToBuffer 7<CR>')
-keymap('n', ',8', ':BufferLineGoToBuffer 8<CR>')
-keymap('n', ',9', ':BufferLineGoToBuffer 9<CR>')
+keymap('n', '<leader>d', ':bdelete<CR>', opts)
+keymap('n', '<F10>', ':BufferLineCycleNext<CR>', opts)
+keymap('n', '<F9>', ':BufferLineCyclePrev<CR>', opts)
+keymap('n', '<leader>bb', ':BufferLinePick<CR>', opts)
+keymap('n', ',d', ':b#<bar>bd#<CR>', opts)
+keymap('n', ',1', ':BufferLineGoToBuffer 1<CR>', opts)
+keymap('n', ',2', ':BufferLineGoToBuffer 2<CR>', opts)
+keymap('n', ',3', ':BufferLineGoToBuffer 3<CR>', opts)
+keymap('n', ',4', ':BufferLineGoToBuffer 4<CR>', opts)
+keymap('n', ',5', ':BufferLineGoToBuffer 5<CR>', opts)
+keymap('n', ',6', ':BufferLineGoToBuffer 6<CR>', opts)
+keymap('n', ',7', ':BufferLineGoToBuffer 7<CR>', opts)
+keymap('n', ',8', ':BufferLineGoToBuffer 8<CR>', opts)
+keymap('n', ',9', ':BufferLineGoToBuffer 9<CR>', opts)
 
 -- moving in wrap
 keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- better indent
-keymap('x', '<', "<gv")
-keymap('x', '>', ">gv")
+keymap('x', '<', "<gv", opts)
+keymap('x', '>', ">gv", opts)
 
 -- move selected line(s)
-keymap('x', 'J', ":move \'>+1<CR>gv-gv")
-keymap('x', 'K', ":move \'<-2<CR>gv-gv")
+keymap('x', 'J', ":move \'>+1<CR>gv-gv", opts)
+keymap('x', 'K', ":move \'<-2<CR>gv-gv", opts)
 
 -- scape visual mode
-keymap('x', 'ii', "<ESC>")
+keymap('x', 'ii', "<ESC>", opts)
 
 -- alternative esc
-keymap('i', 'kk', "<Esc>")
+keymap('i', 'kk', "<Esc>", opts)
 
 -- navigate display lines
-keymap('i', '<Down>', "<Esc>gja")
-keymap('i', '<Up>', "<Esc>gka")
+keymap('i', '<Down>', "<Esc>gja", opts)
+keymap('i', '<Up>', "<Esc>gka", opts)
 
 -- escape in terminal
 keymap('t', '<Esc>', [[<C-\><C-n>]])
 
 -- open dashboard
-keymap('n', '<leader>a', ':Dashboard<CR>', {})
+keymap('n', '<leader>a', ':Dashboard<CR>', opts)
 
 -- open emmet
-keymap('n', '<leader>y', ":Emmet ", {})
+keymap('n', '<leader>y', ":Emmet ", opts)
 
 -- open glow
-keymap('n', '<leader>,', ':Glow<CR>', {})
+keymap('n', '<leader>,', ':Glow<CR>', opts)
 
 -- files explores
-keymap('n', '<leader>ff', ":lua require('telescope.builtin').find_files()<cr>", {})
-keymap('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<cr>", {})
-keymap('n', '<leader>fo', ":lua require('telescope.builtin').oldfiles()<cr>", {})
-keymap('n', '<leader>fk', ":lua require('telescope.builtin').keymaps()<cr>", {})
-keymap('n', '<leader>ft', ":Telescope <CR>", {})
-keymap('n', '<leader>fz', ":lua require('fzf-lua').builtin()<CR>", {})
-keymap('n', '<leader>fm', ":lua require('fzf-lua').files()<CR>", {})
-keymap('n', '<C-n>', ":Neotree<CR>", {})
-keymap('n', '<leader>fb', ":Neotree buffers<CR>", {})
+keymap('n', '<leader>ff', ":lua require('telescope.builtin').find_files()<cr>", opts)
+keymap('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap('n', '<leader>fo', ":lua require('telescope.builtin').oldfiles()<cr>", opts)
+keymap('n', '<leader>fk', ":lua require('telescope.builtin').keymaps()<cr>", opts)
+keymap('n', '<leader>ft', ":Telescope <CR>", opts)
+keymap('n', '<leader>fz', ":lua require('fzf-lua').builtin()<CR>", opts)
+keymap('n', '<leader>fm', ":lua require('fzf-lua').files()<CR>", opts)
+keymap('n', '<C-n>', ":Neotree<CR>", opts)
+keymap('n', '<leader>fb', ":Neotree buffers<CR>", opts)
 
 -- word search
-keymap('n', '<leader>h', ':HopWord<CR>')
+keymap('n', '<leader>h', ':HopWord<CR>', opts)
 
 -- lsp
-keymap('n', '<leader>m', ':Mason<CR>', {})
-keymap('n', '<leader>lt', ":Trouble<CR>", {})
+keymap('n', '<leader>m', ':Mason<CR>', opts)
+keymap('n', '<leader>lt', ":Trouble<CR>", opts)
 
 -- git
-keymap('n', '<leader>gb', ':lua require"gitsigns".blame_line()<CR>', {})
-keymap('n', '<leader>gl', ':LazyGit<CR>', {})
-keymap('n', '<leader>go', ':Octo actions<CR>', {})
+keymap('n', '<leader>gb', ':lua require"gitsigns".blame_line()<CR>', opts)
+keymap('n', '<leader>gl', ':LazyGit<CR>', opts)
+keymap('n', '<leader>go', ':Octo actions<CR>', opts)
 
 -- notes
-keymap('n', '<leader>nd', ":!rm -rf note.md<CR>", {})
-keymap('n', '<leader>no', ":e note.md<CR>", {})
+keymap('n', '<leader>nd', ":!rm -rf note.md<CR>", opts)
+keymap('n', '<leader>no', ":e note.md<CR>", opts)
 
 -- packer
-keymap('n', '<leader>ps', ':PackerSync<CR>', {})
-keymap('n', '<leader>pi', ':PackerInstall<CR>', {})
-keymap('n', '<leader>pu', ':PackerUpdate<CR>', {})
-keymap('n', '<leader>pc', ':PackerClean<CR>', {})
+keymap('n', '<leader>ps', ':PackerSync<CR>', opts)
+keymap('n', '<leader>pi', ':PackerInstall<CR>', opts)
+keymap('n', '<leader>pu', ':PackerUpdate<CR>', opts)
+keymap('n', '<leader>pc', ':PackerClean<CR>', opts)
 
 -- test
-keymap('n', '<leader>nf', ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>, {}")
-keymap('n', '<leader>nu', ":lua require('neotest').run.run()<CR>, {}")
-keymap('n', '<leader>na', ":lua require('neotest').run.run(vim.fn.getcwd())<CR>, {}")
-keymap('n', '<leader>ns', ":lua require('neotest').summary.open()<CR>, {}")
+keymap('n', '<leader>nf', ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", opts)
+keymap('n', '<leader>nu', ":lua require('neotest').run.run()<CR>", opts)
+keymap('n', '<leader>na', ":lua require('neotest').run.run(vim.fn.getcwd())<CR>", opts)
+keymap('n', '<leader>ns', ":lua require('neotest').summary.open()<CR>", opts)
--- plugin/lsp/init.lua
+++ plugin/lsp/init.lua
@@ -2,87 +2,71 @@ local mason = require("mason")
 local mason_lspc = require("mason-lspconfig")
 local lspc = require("lspconfig")
 local nls = require("null-ls").builtins
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local servers = mason_lspc.get_installed_servers()
 
 mason.setup({
   ui = {
-                icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                }
-        }
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
 })
 
 mason_lspc.setup({
-        ensure_installed = {
-                "sumneko_lua",
-                "solargraph",
-                "tsserver",
-        },
+  ensure_installed = {
+    "sumneko_lua",
+    "solargraph",
+    "tsserver",
+  },
 
-        automactic_installation = true,
+  automactic_installation = true,
 })
 
 -- Mappings.
-local keymap = vim.keymap.set
-
-local opts = { noremap = true, silent = true }
 keymap('n', '<space>e', vim.diagnostic.open_float, opts)
 keymap('n', '[d', vim.diagnostic.goto_prev, opts)
 keymap('n', ']d', vim.diagnostic.goto_next, opts)
 keymap('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-        local vlsp = vim.lsp.buf
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local vlsp = vim.lsp.buf
 
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        keymap('n', '<leader>lD', vlsp.declaration, bufopts)
-        keymap('n', '<leader>ld', vlsp.definition, bufopts)
-        keymap('n', 'K', vlsp.hover, bufopts)
-        keymap('n', '<leader>li', vlsp.implementation, bufopts)
-        keymap('n', '<C-k>', vlsp.signature_help, bufopts)
-        keymap('n', '<leader>la', vlsp.add_workspace_folder, bufopts)
-        keymap('n', '<leader>lr', vlsp.remove_workspace_folder, bufopts)
-        keymap('n', '<leader>ll', function()
-                print(vim.inspect(vlsp.list_workspace_folders()))
-        end, bufopts)
-        keymap('n', '<leader>lt', vlsp.type_definition, bufopts)
-        keymap('n', '<leader>lr', vlsp.rename, bufopts)
-        keymap('n', '<leader>lc', vlsp.code_action, bufopts)
-        keymap('n', 'gr', vlsp.references, bufopts)
-        keymap('n', '<leader>lf', vlsp.formatting, bufopts)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  keymap('n', '<leader>lD', vlsp.declaration, bufopts)
+  keymap('n', '<leader>ld', vlsp.definition, bufopts)
+  keymap('n', 'K', vlsp.hover, bufopts)
+  keymap('n', '<leader>li', vlsp.implementation, bufopts)
+  keymap('n', '<C-k>', vlsp.signature_help, bufopts)
+  keymap('n', '<leader>la', vlsp.add_workspace_folder, bufopts)
+  keymap('n', '<leader>lr', vlsp.remove_workspace_folder, bufopts)
+  keymap('n', '<leader>ll', function()
+    print(vim.inspect(vlsp.list_workspace_folders()))
+  end, bufopts)
+  keymap('n', '<leader>lt', vlsp.type_definition, bufopts)
+  keymap('n', '<leader>lr', vlsp.rename, bufopts)
+  keymap('n', '<leader>lc', vlsp.code_action, bufopts)
+  keymap('n', 'gr', vlsp.references, bufopts)
+  keymap('n', '<leader>lf', vlsp.formatting, bufopts)
 end
-
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-local sources = {}
 
 lspc.sumneko_lua.setup {
-        on_attach = on_attach,
-        settings = {
-                lua = {
-                        runtime = {
-                                version = "Lua 5.4",
-                                path = { '~/.config/nvim/init.lua' },
-                        },
-                        format = {
-                                enable = true,
-                                defaultconfig = {
-                                        indent_style = "space",
-                                        indent_size = "2",
-                                }
-                        }
-                }
-        }
+  settings = {
+    lua = {
+      runtime = {
+        version = "Lua 5.4",
+        path = { '~/.config/nvim/init.lua' },
+      },
+    }
+  }
 }
-lspc.solargraph.setup {
-        on_attach = on_attach,
-}
-lspc.tsserver.setup {
-        on_attach = on_attach,
-}
+
+for _, server in ipairs(servers) do
+  lspc[server].setup {
+    on_attach = on_attach
+  }
+end
