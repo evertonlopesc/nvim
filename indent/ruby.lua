6e55e0ba24eaa48c6dbdebd3fd6776f6bbdf1a71
Everton Lopes <everton.lopess@outlook.com>
2022-10-02 17:45:22 -0300

    refactor(ruby): textwidth removing.

--- indent/ruby.lua
+++ indent/ruby.lua
@@ -1,3 +1,2 @@
-vim.opt.colorcolumn = "120"
-vim.opt.textwidth = 120
-vim.opt.indentkeys = "0{,0},0), 0], !^F, o, O, e, :, =end, =else, =elsif, =when, =in, =ensure, =rescue, ==begin, ==end, =private, =protected, =public"
+vim.opt.indentkeys =
+  "0{,0},0), 0], !^F, o, O, e, :, =end, =else, =elsif, =when, =in, =ensure, =rescue, ==begin, ==end, =private, =protected, =public"
