963f8eca989d9b9d3a0edb433f4d185a46fe26af
Everton Lopes <everton.lopess@outlook.com>
2022-10-02 17:28:20 -0300

    refactor: simplifying lualine.

--- plugin/bootstrap/lualine.lua
+++ plugin/bootstrap/lualine.lua
@@ -1,56 +1,3 @@
-local colors = {
-  red = "#ca1243",
-  grey = "#a0a1a7",
-  black = "#383a42",
-  white = "#f3f3f3",
-  light_green = "#83a598",
-  orange = "#fe8019",
-  green = "#8ec07c",
-  blue = "#45AFF8",
-  purple = "#9B45F8",
-}
-
-local theme = {
-  normal = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.white },
-    z = { fg = colors.white, bg = colors.black },
-  },
-  insert = { a = { fg = colors.black, bg = colors.light_green } },
-  visual = { a = { fg = colors.black, bg = colors.orange } },
-  replace = { a = { fg = colors.black, bg = colors.green } },
-  terminal = { a = { fg = colors.white, bg = colors.blue } },
-  command = { a = { fg = colors.white, bg = colors.purple } },
-}
-
-local empty = require("lualine.component"):extend()
-function empty:draw(default_highlight)
-  self.status = ""
-  self.applied_separator = ""
-  self:apply_highlights(default_highlight)
-  self:apply_section_separators()
-  return self.status
-end
-
--- Put proper separators and gaps between components in sections
-local function process_sections(sections)
-  for name, section in pairs(sections) do
-    local left = name:sub(9, 10) < "x"
-    for pos = 1, name ~= "lualine_z" and #section or #section - 1 do
-      table.insert(section, pos * 2, { empty, color = { fg = colors.white, bg = colors.white } })
-    end
-    for id, comp in ipairs(section) do
-      if type(comp) ~= "table" then
-        comp = { comp }
-        section[id] = comp
-      end
-      comp.separator = left and { right = "" } or { left = "" }
-    end
-  end
-  return sections
-end
-
 local function lsp_server_name()
   local msg = "none"
   local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
@@ -67,62 +14,20 @@ local function lsp_server_name()
   return msg
 end
 
-local function search_result()
-  if vim.v.hlsearch == 0 then
-    return ""
-  end
-  local last_search = vim.fn.getreg "/"
-  if not last_search or last_search == "" then
-    return ""
-  end
-  local searchcount = vim.fn.searchcount { maxcount = 9999 }
-  return last_search .. "(" .. searchcount.current .. "/" .. searchcount.total .. ")"
-end
-
-local function modified()
-  if vim.bo.modified then
-    return "+"
-  elseif vim.bo.modifiable == false or vim.bo.readonly == true then
-    return "-"
-  end
-  return ""
-end
-
 require("lualine").setup {
   options = {
-    theme = theme,
+    theme = "onedark",
     component_separators = "",
     section_separators = { left = "", right = "" },
   },
-  sections = process_sections {
+  sections = {
     lualine_a = { "mode" },
     lualine_b = {
       {
         "filename",
         icon = " ",
         file_status = true,
-        path = 1,
-      },
-      "branch",
-      "diff",
-      {
-        "diagnostics",
-        source = { "nvim" },
-        sections = { "error" },
-        diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
-      },
-      {
-        "diagnostics",
-        source = { "nvim" },
-        sections = { "warn" },
-        diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
-      },
-      { modified, color = { bg = colors.red } },
-      {
-        "%w",
-        cond = function()
-          return vim.wo.previewwindow
-        end,
+        path = 0,
       },
       {
         icon = "",
@@ -131,20 +36,18 @@ require("lualine").setup {
           return vim.bo.readonly
         end,
       },
-      {
-        "%q",
-        cond = function()
-          return vim.bo.buftype == "quickfix"
-        end,
-      },
+      "branch",
+      "diff",
+      "diagnostics",
     },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = { search_result, { icon = "力", lsp_server_name }, "filetype" },
-    lualine_z = { "encoding", "%l:%c", "%p%%/%L" },
+    lualine_y = { { icon = "力", lsp_server_name }, "filetype" },
+    lualine_z = { "encoding", "location", "%p%%/%L" },
   },
   inactive_sections = {
     lualine_c = { "%f %y %m" },
     lualine_x = {},
   },
+  extensions = { "toggleterm", "neo-tree", "quickfix" },
 }
