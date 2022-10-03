bf8e816b04fd6b2380027a00a009bfd446d81298
Everton Lopes <everton.lopess@outlook.com>
2022-10-02 22:51:43 -0300

    feat(neogit): added keymaps to commit, push and pull.

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -2,4 +2,6 @@ local neogit = require "neogit"
 
 neogit.setup {}
 
-vim.keymap.set("n", "<leader>gc", "<cmd> Neogit commit <cr>")
+vim.keymap.set("n", "<leader>ic", "<cmd> Neogit commit <cr>")
+vim.keymap.set("n", "<leader>ip", "<cmd> Neogit push <cr>")
+vim.keymap.set("n", "<leader>iu", "<cmd> Neogit pull <cr>")
