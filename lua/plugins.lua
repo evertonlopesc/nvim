ead073e54881671c6c58c2da7275b68ce549ba8c
Everton Lopes <everton.lopess@outlook.com>
2022-10-12 18:11:06 -0300

    Added diffview.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -137,6 +137,10 @@ startup(function()
   }
 
   use { "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" }
+  use {
+    "sindrets/diffview.nvim",
+    requires = { "nvim-lua/plenary.nvim", "kyazdani42/nvim-web-devicons" },
+  }
 
   -- Comment
   use {
--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -1,7 +1,13 @@
 local neogit = require "neogit"
 
-neogit.setup {}
+neogit.setup {
+  integrations = {
+    diffview = true,
+  },
+}
 
+vim.keymap.set("n", "<leader>ii", "<cmd> Neogit <cr>")
+vim.keymap.set("n", "<leader>id", "<cmd> Neogit diff <cr>")
 vim.keymap.set("n", "<leader>ic", "<cmd> Neogit commit <cr>")
-vim.keymap.set("n", "<leader>ip", "<cmd> Neogit push <cr>")
-vim.keymap.set("n", "<leader>iu", "<cmd> Neogit pull <cr>")
+vim.keymap.set("n", "<leader>is", "<cmd> Neogit push <cr>")
+vim.keymap.set("n", "<leader>il", "<cmd> Neogit pull <cr>")
