f07906a61e1e0ca8bc0052a3b771cab02c1fd01f
Everton Lopes <everton.lopess@outlook.com>
2022-11-09 22:14:05 -0300

    added catppuccin configuration

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,35 @@
 require("catppuccin").setup({
+  flavour = "latte", -- latte, frappe, macchiato, mocha
+  background = {
+    light = "latte",
+    dark = "mocha",
+  },
   transparent_background = true,
+  styles = {
+    comments = { "italic" },
+    conditionals = {},
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
   integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    telescope = true,
+    treesitter = true,
     neogit = true,
     hop = true,
     dashboard = true,
   },
 })
 
-vim.opt.background = "dark"
-vim.cmd("colorscheme catppuccin")
-
-print("Hello colors")
+vim.opt.background = "light"
+vim.api.nvim_command("colorscheme catppuccin")
