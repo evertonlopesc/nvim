e7d24f18611ad93058dfed1d32eb81c2a34d1dd3
Everton Lopes <everton.lopess@outlook.com>
2022-11-29 18:47:50 -0300

    feat: add onedarkpro colorscheme config.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -31,5 +31,22 @@ require("catppuccin").setup({
   },
 })
 
-vim.opt.background = "light"
-vim.api.nvim_command("colorscheme catppuccin")
+require("onedarkpro").setup({
+  light_theme = "onelight",
+  styles = { -- Choose from "bold,italic,underline"
+    types = "NONE", -- Style that is applied to types
+    numbers = "NONE", -- Style that is applied to numbers
+    strings = "NONE", -- Style that is applied to strings
+    comments = "NONE", -- Style that is applied to comments
+    keywords = "NONE", -- Style that is applied to keywords
+    constants = "NONE", -- Style that is applied to constants
+    functions = "NONE", -- Style that is applied to functions
+    operators = "NONE", -- Style that is applied to operators
+    variables = "NONE", -- Style that is applied to variables
+    conditionals = "NONE", -- Style that is applied to conditionals
+    virtual_text = "NONE", -- Style that is applied to virtual text
+  },
+})
+
+vim.opt.background = "dark"
+vim.api.nvim_command("colorscheme nightfox")
