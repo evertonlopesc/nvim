85ba25f2aceead379404e5fea7a6d34c8f0d0ae8
Everton Lopes <everton.lopess@outlook.com>
2022-12-17 17:45:00 -0300

    chore: move options from init to lua_options.

--- init.lua
+++ init.lua
@@ -1,70 +1,7 @@
-require("plugins")
-require("lsp.progress_update")
+local status, plugins = pcall(require, "plugins")
+local status, options = pcall(require, "options")
+local status, _ = pcall(require, "lsp.progress_update")
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Options summary
-vim.opt.autoindent = true
-vim.opt.backup = false
-vim.opt.clipboard = "unnamedplus"
-vim.opt.colorcolumn = "80,120"
-vim.opt.confirm = true
-vim.opt.cursorline = true
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.encoding = "utf-8"
-vim.opt.expandtab = true
-vim.opt.ffs = "unix,mac,dos"
-vim.opt.fileencoding = "utf-8"
-vim.opt.fileencodings = "utf-8"
-vim.opt.foldlevelstart = 99
-vim.opt.hidden = true
-vim.opt.hlsearch = true
-vim.opt.history = 1000
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.laststatus = 3
-vim.opt.linebreak = true
-vim.opt.maxmempattern = 5000
-vim.opt.mouse = "a"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 5
-vim.opt.shiftwidth = 2
-vim.opt.sidescrolloff = 5
-vim.opt.signcolumn = "yes"
-vim.opt.spell = true
-vim.opt.spelllang = "en_us"
-vim.opt.swapfile = false
-vim.opt.syntax = "enable"
-vim.opt.smartcase = true
-vim.opt.startofline = true
-vim.opt.tabstop = 2
-vim.opt.termguicolors = true
-vim.opt.textwidth = 80, 120
-vim.opt.timeoutlen = 300
-vim.opt.undodir = os.getenv("HOME" .. "/home/everton/.config/.vim/undodir")
-vim.opt.undofile = true
-vim.opt.wig = {
-  "**/node_module/*",
-  "**/coverage/*",
-  "**/.git/*",
-  ".pyc",
-  ".swp",
-  "*.o",
-  "*.obj",
-  "*~",
-  "*vim/backups",
-  "*sass-cache",
-  "*DS_Store",
-  "vendor/rails/**",
-  "vendor/cache/**",
-  "*.gem",
-  "log/**",
-  "tmp/**",
-  "*.png",
-  "*.jpg",
-  "*.gif",
-  "*.swp",
-  "*.pyc"
-}
+if not status then
+  return
+end
