1c9c8d8a25836567f18d027880cdd39caf5a138a
evertonlc <everton.lopess@outlook.com>
2023-01-21 20:08:05 -0300

    change keymaps configuration.

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,35 +1,194 @@
 local keymap = vim.keymap.set
-local opts = { noremap = true, silent = false }
 
-keymap('n', '<leader>rr', ':source % <CR>', opts)
-keymap('n', '<leader>S', ':%s/<c-r><c-w>//g<left><left>', opts)
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true }, opts)
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true }, opts)
-keymap('n', '<Down>', ':resize -5<CR>', opts)
-keymap('n', '<Up>', ':resize +5<CR>', opts)
-keymap('n', '<Left>', ':vertical resize -5<CR>', opts)
-keymap('n', '<Right>', ':vertical resize +5<CR>', opts)
-keymap('n', '<C-k>', '<cmd>bnext<CR>', opts)
-keymap('n', '<C-j>', '<cmd>bprevious<CR>', opts)
-keymap('n', '<A-h>', '<C-w>h', opts)
-keymap('n', '<A-j>', '<C-w>j', opts)
-keymap('n', '<A-k>', '<C-w>k', opts)
-keymap('n', '<A-l>', '<C-w>l', opts)
-keymap('n', '<leader><space>', ':set hlsearch!<CR>', opts)
-keymap('n', '<leader>ca', 'ggVGy', opts)
-keymap('n', '<leader>d', ':bdelete<CR>', opts)
-keymap('n', '<leader>ss', ':HopWord<CR>', opts)
-keymap('n', '<leader>nd', ':!rm -rf note.md<CR>', opts)
-keymap('n', '<leader>no', ':e note.md<CR>', opts)
-keymap('n', '<leader>x', ':q<CR>', opts)
-keymap('n', '<leader>X', ':qa<CR>', opts)
-keymap('n', '<leader>w', ':w<CR>', opts)
-keymap('n', '<leader>W', ':wa<CR>', opts)
-keymap('n', '<leader>u', ':ma ', opts)
-keymap('n', '<leader>h', ':vert help ', opts)
-keymap('n', '<leader>n', ':Lazy<CR>', opts)
-keymap('x', '<', '<gv', opts)
-keymap('x', '>', '>gv', opts)
-keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
-keymap('x', 'K', ":move '<-2<CR>gv-gv", opts)
-keymap('i', 'kk', '<Esc>', opts)
+keymap(
+  'n',
+  '<leader>rr',
+  ':source % <CR>',
+  { desc = 'Refresh', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>S',
+  ':%s/<c-r><c-w>//g<left><left>',
+  { desc = 'Replace cursor all', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  'k',
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, noremap = true, silent = false }
+)
+keymap(
+  'n',
+  'j',
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<Down>',
+  ':resize -5<CR>',
+  { desc = 'Resize -5', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<Up>',
+  ':resize +5<CR>',
+  { desc = 'Resize +5', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<Left>',
+  ':vertical resize -5<CR>',
+  { desc = 'Vertical resize -5', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<Right>',
+  ':vertical resize +5<CR>',
+  { desc = 'Vertical resize +5', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<C-k>',
+  '<cmd>bnext<CR>',
+  { desc = 'Next buffer', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<C-j>',
+  '<cmd>bprevious<CR>',
+  { desc = 'Previous buffer', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<A-h>',
+  '<C-w>h',
+  { desc = 'Move windows left', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<A-j>',
+  '<C-w>j',
+  { desc = 'Move windows down', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<A-k>',
+  '<C-w>k',
+  { desc = 'Move windows up', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<A-l>',
+  '<C-w>l',
+  { desc = 'Move windows right', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader><space>',
+  ':set hlsearch!<CR>',
+  { desc = 'Highlight', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>ca',
+  'ggVGy',
+  { desc = 'Copy all', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>d',
+  ':bdelete<CR>',
+  { desc = 'Delete buffer', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>ss',
+  ':HopWord<CR>',
+  { desc = 'Hop word', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>nd',
+  ':!rm -rf note.md<CR>',
+  { desc = 'Delete note', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>no',
+  ':e note.md<CR>',
+  { desc = 'Open note', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>x',
+  ':q<CR>',
+  { desc = 'Exit', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>X',
+  ':qa<CR>',
+  { desc = 'Exit without save', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>w',
+  ':w<CR>',
+  { desc = 'Save', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>W',
+  ':wa<CR>',
+  { desc = 'Save all', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>u',
+  ':ma ',
+  { desc = 'Create mark', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>h',
+  ':vert help ',
+  { desc = 'Open help', noremap = true, silent = false }
+)
+keymap(
+  'n',
+  '<leader>n',
+  ':Lazy<CR>',
+  { desc = 'Lazy', noremap = true, silent = false }
+)
+keymap(
+  'x',
+  '<',
+  '<gv',
+  { desc = '', noremap = true, silent = false }
+)
+keymap(
+  'x',
+  '>',
+  '>gv',
+  { desc = '', noremap = true, silent = false }
+)
+keymap(
+  'x',
+  'J',
+  ":move '>+1<CR>gv-gv",
+  { desc = 'Move line down', noremap = true, silent = false }
+)
+keymap(
+  'x',
+  'K',
+  ":move '<-2<CR>gv-gv",
+  { desc = 'Move line up', noremap = true, silent = false }
+)
+keymap(
+  'i',
+  'kk',
+  '<Esc>',
+  { desc = 'Esc insert mode', noremap = true, silent = false }
+)
