e0cdd763392e1b54cb12da14c021db01bdb45394
evertonlc <everton.lopess@outlook.com>
2023-01-22 10:59:51 -0300

    Fix lsp server installation.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,15 +9,15 @@ local M = {
         opts = {
           ui = {
             icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗"
+              package_installed = '✓',
+              package_pending = '➜',
+              package_uninstalled = '✗',
             },
           },
         },
         config = true,
         keys = {
-          { '<leader>m', ':Mason<cr>', desc = 'Mason' }
+          { '<leader>m', ':Mason<cr>', desc = 'Mason' },
         },
         cmd = 'Mason',
       },
@@ -26,16 +26,49 @@ local M = {
         event = 'BufReadPost',
         opts = {
           ensure_installed = {
-            "sumneko_lua", "solargraph", 'tsserver',
+            'sumneko_lua',
+            'solargraph',
+            'tsserver',
           },
         },
         config = true,
       },
     },
     config = function()
-      require("mason").setup()
-      local masonlsp = require("mason-lspconfig")
-      local lspconfig = require("lspconfig")
+      require('mason').setup()
+      local masonlsp = require('mason-lspconfig')
+      local lspconfig = require('lspconfig')
+
+      local signs =
+        { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      local border = {
+        { '╭', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '╮', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+        { '╯', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '╰', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+      }
+
+      local handlers = {
+        ['textDocument/hover'] = vim.lsp.with(
+          vim.lsp.handlers.hover,
+          { border = border }
+        ),
+        ['textDocument/signatureHelp'] = vim.lsp.with(
+          vim.lsp.handlers.signature_help,
+          { border = border }
+        ),
+      }
+
+      vim.diagnostic.config({ virtual_text = false })
 
       local opts = { noremap = true, silent = true }
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
@@ -43,14 +76,12 @@ local M = {
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
       vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-      vim.diagnostic.config({ virtual_text = false })
-
       local on_attach = function(_, bufnr)
         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', '<leader>lc', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', '<leader>ld', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', '<leader>lc', vim.lsp.buf.definition, bufopts)
         vim.keymap.set('n', '<leader>lk', vim.lsp.buf.hover, bufopts)
         vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
         vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, bufopts)
@@ -58,67 +89,58 @@ local M = {
         vim.keymap.set('n', '<leader>lrn', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
+        vim.keymap.set('n', '<leader>lf', function()
+          vim.lsp.buf.format({ async = true })
+        end, bufopts)
       end
 
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local function make_config()
-        return { on_attach = on_attach, capabilities = capabilities }
+        return {
+          on_attach = on_attach,
+          capabilities = capabilities,
+          handlers = handlers,
+        }
       end
 
       masonlsp.setup()
-      masonlsp.setup_handlers {
+      masonlsp.setup_handlers({
         function(server_name)
-          lspconfig[server_name].setup { make_config() }
+          lspconfig[server_name].setup({ make_config() })
         end,
 
-        ["sumneko_lua"] = function()
+        ['sumneko_lua'] = function()
           local config = make_config()
-          config.settings = {
-            Lua = {
-              runtime = {
-                version = 'LuaJIT',
-              },
-              diagnostics = {
-                globals = { 'vim' },
-              },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file('', true),
-              },
-              telemetry = {
-                enable = false,
-              },
-            }
-          }
+          config.settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
 
           lspconfig.sumneko_lua.setup(config)
         end,
 
-        ["solargraph"] = function()
+        ['solargraph'] = function()
           local config = make_config()
 
           lspconfig.solargraph.setup(config)
         end,
 
-        ["grammarly"] = function()
+        ['marksman'] = function()
           local config = make_config()
 
-          lspconfig.grammarly.setup(config)
+          lspconfig.marksman.setup(config)
         end,
 
-        ["tsserver"] = function()
+        ['tsserver'] = function()
           local config = make_config()
 
           lspconfig.tsserver.setup(config)
         end,
 
-        ["vue"] = function()
+        ['volar'] = function()
           local config = make_config()
 
-          lspconfig.vue.setup(config)
+          lspconfig.volar.setup(config)
         end,
-      }
+      })
     end,
   },
 
@@ -134,14 +156,45 @@ local M = {
 
       nls.setup({
         sources = {
-          blts.formatting.erb_lint,
-          blts.formatting.rubocop,
-          blts.formatting.stylua,
-          blts.formatting.prettierd,
-          blts.formatting.markdownlint,
+          blts.formatting.erb_lint.with({
+            filetypes = { 'erb' },
+          }),
+          blts.formatting.rubocop.with({
+            filetypes = { 'rb' },
+          }),
+          blts.formatting.stylua.with({
+            filetypes = { 'lua' },
+          }),
+          blts.formatting.prettierd.with({
+            filetypes = {
+              'javascript',
+              'javascriptreact',
+              'typescript',
+              'typescriptreact',
+              'vue',
+              'css',
+              'scss',
+              'less',
+              'html',
+              'json',
+              'jsonc',
+              'yaml',
+            },
+          }),
+          blts.formatting.markdownlint.with({
+            filetypes = { 'markdown' },
+          }),
 
           blts.code_actions.gitsigns,
-          blts.code_actions.eslint_d,
+          blts.code_actions.eslint_d.with({
+            filetypes = {
+              'javascript',
+              'javascriptreact',
+              'typescript',
+              'typescriptreact',
+              'vue',
+            },
+          }),
         },
       })
     end,
