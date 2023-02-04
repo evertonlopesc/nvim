4c3f9cde24a8890ccd769ae3d451fc68ebf3bb23
evertonlc <everton.lopess@outlook.com>
2023-02-04 07:46:49 -0300

    theme: add github plugin and configuration.

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,5 +2,13 @@ require('config.lazy')
 require('config.options')
 require('config.auto_command')
 
-vim.opt.background = 'dark'
-vim.cmd.colorscheme('rose-pine')
+vim.opt.background = 'light'
+-- vim.cmd.colorscheme('rose-pine')
+
+require('github-theme').setup({
+  theme_style = 'light',
+  comment_style = "italic",
+  keyword_style = "bold",
+  function_style = "bold",
+  variable_style = "NONE"
+})
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,24 +2,22 @@ local M = {
   {
     'norcalli/nvim-colorizer.lua',
     event = 'BufReadPost',
-    config = function ()
-      require'colorizer'.setup()
+    config = function()
+      require('colorizer').setup()
     end,
   },
 
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
+    enable = false,
     opts = {
       dark_variant = 'moon',
       bold_vert_split = false,
       dim_nc_background = false,
       disable_background = false,
       disable_float_background = false,
-      disable_italics = true,
-
+      disable_italics = false,
       groups = {
         background = 'base',
         panel = 'surface',
@@ -27,12 +25,10 @@ local M = {
         comment = 'muted',
         link = 'iris',
         punctuation = 'subtle',
-
         error = 'love',
         hint = 'iris',
         info = 'foam',
         warn = 'gold',
-
         headings = {
           h1 = 'iris',
           h2 = 'foam',
@@ -42,12 +38,18 @@ local M = {
           h6 = 'foam',
         },
       },
-
       highlight_groups = {
         ColorColumn = { bg = 'rose' },
       },
     },
     config = true,
+  },
+
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    priority = 1000,
+    version = 'v0.0.7',
   },
 }
 
