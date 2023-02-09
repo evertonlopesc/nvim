8fc3cf45f07c314777d7d2c4f6f84b66feb5b505
evertonlc <everton.lopess@outlook.com>
2023-02-09 17:55:54 -0300

    chore: add wich_key plugin.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -63,6 +63,19 @@ local M = {
     },
     cmd = 'Neotest',
   },
+
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
 }
 
 return M
