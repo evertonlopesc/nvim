31d1a30c4a7f794a5997976d48075adb60e1c8ca
evertonlc <everton.lopess@outlook.com>
2023-02-23 06:42:02 -0300

    indent: fix adding the smatindent options, based C language.

--- indent/ruby.lua
+++ indent/ruby.lua
@@ -1,2 +1,2 @@
-vim.opt.indentkeys =
-  '0{,0},0), 0], !^F, o, O, e, :, =end, =else, =elsif, =when, =in, =ensure, =rescue, ==begin, ==end, =private, =protected, =public'
+-- vim.opt.indentkeys =
+  -- '0{,0},0), 0], !^F, o, O, e, :, =end, =else, =elsif, =when, =in, =ensure, =rescue, ==begin, ==end, =private, =protected, =public'
