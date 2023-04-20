b4ef8c968b2112c203245b2cea31f899c3178df6
evertonlc <everton.lopess@outlook.com>
2023-04-19 21:08:13 -0300

    chore: refactor theme configuration.

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,4 +1,4 @@
 require('config.lazy')
 require('config.options')
 require('config.autocommands')
-require('config.theme').set_theme_color()
+require('config.theme').set_color_theme()
--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,20 +1,23 @@
 local M = {}
 
-function M.get_theme_color()
+function M.get_color_theme()
   local command = 'gsettings get org.gnome.desktop.interface gtk-theme'
-  local result = string.find(vim.fn.system(command), 'dark')
 
-  return result
+  if string.find(vim.fn.system(command), 'dark') == nil then
+    return 'light'
+  else
+    return 'dark'
+  end
 end
 
-function M.set_theme_color()
-  if M.get_theme_color() == nil then
-    vim.opt.background = 'light'
-    require('github-theme').setup({
-      theme_style = 'light',
-    })
+function M.set_color_theme()
+  local background = M.get_color_theme()
+
+  if background == 'light' then
+    vim.opt.background = background
+    require('github-theme').setup({ theme_style = background })
   else
-    vim.opt.background = 'dark'
+    vim.opt.background = background
     vim.cmd.colorscheme('nightfox')
   end
 end
