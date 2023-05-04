388e2b2b5de3422987dc1912ad09f0f49a1fe097
evertonlc <everton.lopess@outlook.com>
2023-05-04 09:45:31 -0300

    refactor: timenout to 2000 and clear which-key plugin.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -65,15 +65,7 @@ local M = {
 
   {
     'folke/which-key.nvim',
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require('which-key').setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
+    config = true
   },
 
   {
