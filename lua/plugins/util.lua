ae96f6d1be0368ee5135c09f67ba92b5a19e5819
evertonlopesc <everton.lopess@outlook.com>
2023-10-19 07:53:07 -0300

    chore: add config for dap-nvim to ruby.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -68,8 +68,42 @@ M = {
   {
     'mfussenegger/nvim-dap',
     dependencies = {
-       'suketa/nvim-dap-ruby',
+      'suketa/nvim-dap-ruby',
+      config = function()
+        local dap = require("dap")
+        dap.adapters.ruby = function(callback, config)
+          callback {
+            type = "server",
+            host = "127.0.0.1",
+            port = "${port}",
+            executable = {
+              command = "bundle",
+              args = { "exec", "rdbg", "-n", "--open", "--port", "${port}",
+                "-c", "--", "bundle", "exec", config.command, config.script,
+              },
+            },
+          }
+        end
 
+        dap.configurations.ruby = {
+          {
+            type = "ruby",
+            name = "debug current file",
+            request = "attach",
+            localfs = true,
+            command = "ruby",
+            script = "${file}",
+          },
+          {
+            type = "ruby",
+            name = "run current spec file",
+            request = "attach",
+            localfs = true,
+            command = "rspec",
+            script = "${file}",
+          },
+        }
+      end
     },
     ft = { 'rb', 'erb' },
     keys = {
