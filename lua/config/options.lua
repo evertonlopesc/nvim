9446e70d172cf65619a54a03565635688d2b0940
evertonlopesc <everton.lopess@outlook.com>
2024-03-06 09:36:01 -0300

    refactor: change column options to 80.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@ local opt = vim.opt
 local o = vim.o
 
 -- [[ Context ]]
-opt.colorcolumn = '100,120,140' -- str: Show col for max line length
+opt.colorcolumn = '80,120,140' -- str: Show col for max line length
 opt.number = true           -- bool: Show line numbers
 opt.relativenumber = true   -- bool: Show relative line numbers
 opt.scrolloff = 4           -- int: Min num lines of context
