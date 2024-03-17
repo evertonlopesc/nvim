be81b971da5e723535affb917c3f8d10b264a73c
evertonlopesc <everton.lopess@outlook.com>
2024-03-17 20:08:21 -0300

    lsp: add emmet-language-server.

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -8,7 +8,7 @@ M = {
         'williamboman/mason-lspconfig.nvim',
         opts = {
           ensure_installed = {
-            'lua_ls', 'solargraph', 'grammarly', 'tsserver', 'rust-analyzer'
+            'lua_ls', 'solargraph', 'grammarly', 'tsserver', 'rust-analyzer', 'emmet-language-server'
           },
           automatic_installation = true,
         },
@@ -46,6 +46,31 @@ M = {
             on_attach = on_attach, capabilities = capabilities, handlers =
               handlers,
           }
+          lspconfig.emmet_language_server.setup({
+            filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+            -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+            -- **Note:** only the options listed in the table are supported.
+            init_options = {
+              ---@type table<string, string>
+              includeLanguages = {},
+              --- @type string[]
+              excludeLanguages = {},
+              --- @type string[]
+              extensionsPath = {},
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+              preferences = {},
+              --- @type boolean Defaults to `true`
+              showAbbreviationSuggestions = true,
+              --- @type "always" | "never" Defaults to `"always"`
+              showExpandedAbbreviation = "always",
+              --- @type boolean Defaults to `false`
+              showSuggestionsAsSnippets = false,
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+              syntaxProfiles = {},
+              --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+              variables = {},
+            },
+          })
         end,
       },
       {
